5b83895e122ccf4a4cdb0491098c0f02bd3a1c6e
mcastellonc201 <castellonmichael@gmail.com>
2022-06-18 12:16:38 -0600

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -4,31 +4,35 @@
 
 int main()
 {
-	system("mode con: cols=100 lines=40");
-	SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
-
+	//system("mode con: cols=16 lines=10");
+	//SendMessage(::GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+	HWND hwnd = GetConsoleWindow();
+	//SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_MINIMIZEBOX);
+	SetWindowLong(hwnd, GWL_STYLE, GetWindowLong(hwnd, GWL_STYLE) & ~WS_MAXIMIZEBOX);
+	EnableMenuItem(GetSystemMenu(hwnd, FALSE), SC_CLOSE, MF_BYCOMMAND | MF_DISABLED | MF_GRAYED);
+	
 	std::string Menu[NOPTIONS] = { 
-		"  COMMAND   ",
-		"  BROWSER   ",
-		"  PROGRAMS  ",
-		"  CONTROL   ",
-		"  REBOOT    ",
-		"  POWEROFF  "
+		"    INTERNET    ",
+		"    PROGRAMS    ",
+		"    PANEL       ",
+		"    COMMAND     ",
+		"    REBOOT      ",
+		"    POWEROFF    "
 	};
 	int index = 0;
 
 	while (true)
 	{
 		system("cls");
-		std::cout << "============\n";
-		std::cout << "    MENU    \n";
-		std::cout << "============\n";
+		std::cout << "================\n";
+		std::cout << "      MENU      \n";
+		std::cout << "================\n";
 
 		for (int i = 0; i < NOPTIONS; ++i)
 		{
 			if (i == index)
 			{
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 128);
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 31);
 				std::cout << Menu[i] << std::endl;
 				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 15);
 			}
@@ -37,7 +41,7 @@ int main()
 				std::cout << Menu[i] << std::endl;
 			}
 		}
-		std::cout << "============\n";
+		std::cout << "================";
 		while (true)
 		{
 			system("pause > null");
@@ -65,32 +69,52 @@ int main()
 				{
 				case 0:
 				{
-					system("start cmd");
+					system("start www.google.com");
 				}
 				break;
 				case 1:
 				{
-					system("start www.google.com");
+					system("start explorer \"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\" ");
 				}
 				break;
 				case 2:
 				{
-					system("start explorer \"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\" ");
+					system("start control");
 				}
 				break;
 				case 3:
 				{
-					system("start control");
+					system("start cmd");
 				}
 				break;
 				case 4:
 				{
-					system("shutdown -r -t 0");
+					char y;
+					do
+					{
+						system("cls");
+						std::cout<< "\n Reboot (Y/N)?\n > ";
+						std::cin >> y;
+						if ((y =='Y')||(y =='y')) {
+							system("shutdown -r -t 0");
+						} 
+					}
+					while((y !='Y')&&(y !='N')&&(y !='y')&&(y !='n'));
 				}
 				break;
 				case 5:
 				{
-					system("shutdown -s -t 0");
+					char y;
+					do
+					{
+						system("cls");
+						std::cout<< "\n Poweroff (Y/N)?\n > ";
+						std::cin >> y;
+						if ((y =='Y')||(y =='y')) {
+							system("shutdown -s -t 0");
+						} 
+					}
+					while((y !='Y')&&(y !='N')&&(y !='y')&&(y !='n'));
 				}
 				break;
 				
